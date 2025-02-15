a8e0ab6601d7727a0d688e9db30e9acacaab9ab4
Som S. <asriv132@asu.edu>
2025-02-15 11:14:40 -0700

    e

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async rewrites() {
+      return [
+        {
+          source: "/api/:path*",
+          destination: `http://localhost:5328/api/:path*`, // Flask backend
+        },
+      ];
+    },
+    // Optional: Add these if using Next.js Image Optimization
+    images: {
+      domains: ["localhost"], // Add your production domain later
+    },
+  };
+  
+  export default nextConfig;
+  
